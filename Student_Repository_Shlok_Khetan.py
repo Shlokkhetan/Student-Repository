cc99306ece027088977f3bd98b624d99aa843a0f
Shlokkhetan <shlok.khetan@gmail.com>
2020-04-10 03:53:30 +0530

    Add files via upload

--- Student_Repository_Shlok_Khetan.py
+++ Student_Repository_Shlok_Khetan.py
@@ -1,139 +1,287 @@
-"""Repository for stevens , author: Shlok Arun Khetan"""
-
-import os
-import unittest
+from HW08_Shlok_Khetan import file_reader
 from collections import defaultdict
 from prettytable import PrettyTable
+from typing import DefaultDict,Dict,Tuple,Iterator,List
+import os 
+class Major:
+    """R or E selective class"""
+    def __init__(self,major: str):
+        self.major: str = major
+        self.required_crse = list()
+        self.elective_crse = list()
+   
+    
+    def add_major(self,course,R) -> None:
+        """list of required and electives are seperated"""
+        if R == 'R':
+            self.required_crse.append(course) 
+        elif R == 'E':zt
+            self.elective_crse.append(course)   
+        else:
+            print(f"ad data found.flag {R} is not working properly")
 
-class Repository(object):
-    """ Repository objects"""
+    def get_required(self) ->List[str]:
+        """ Returns the required and electives list"""
+        return list(self.required_crse)
+    
+    def get_electives(self) -> List[str]:
+        """ electives are returned"""
+        return list(self.elective_crse)
+    
+    def pretty_major(self) -> Tuple[str,str,str]:
+        """pretty tale field is eeturned"""
+        return [self.major,sorted(self.required_crse),sorted(self.elective_crse)]
 
-    def __init__(self, direct):
-        """init method for Repository"""
 
-        self.directory = direct
-        os.chdir(self.directory)
-        self.student_dict = dict()
-        self.instructor_dict = dict()
-        self.majors_dict = defaultdict(lambda: defaultdict(list))
+class Instructor:
+    """ Instructor class"""
+
+   
+    def __init__(self,cwid:str , name:str, dept:str) -> None:
+        self.cwid: str = cwid
+        self.name: str = name
+        self.dpt: str = dept
+        self.stud: DefaultDict[str,int] = defaultdict(int)
 
 
-        student_path = "students.txt"
-        instructor_path = "instructors.txt"
-        grades_path = "grades.txt"
-        majors_path = "majors.txt"
-
-        valid_grades = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C']
-
-        for cwid, name, major in file_reader(student_path, 3, "\t"):
-            self.new_student(cwid, name, major)
-
-        for cwid, name, dept in file_reader(instructor_path, 3, "\t"):
-            self.new_instructor(cwid, name, dept)
-
-        for student_cwid, subject, grade, instructor_cwid in file_reader(grades_path, 4, "\t"):
-            for key, value in self.student_dict.items():
-                if student_cwid == key and grade in valid_grades:
-                    value.courses.append(subject)
-
-            for key, value in self.instructor_dict.items():
-                if instructor_cwid == key:
-                    value.courses[subject] += 1
+    def add_student(self, course:str) -> None:
+        """student dict in innstructors's class"""
+      
        
-        for major, re, course in file_reader(majors_path, 3, "\t", header=True):
-            self.majors_dict[major][re].append(course)
+        self.stud[course] +=1
+    
+    def pretty_instructor(self) -> Iterator[Tuple[str,str,str,str,int]]:
+        """Returns the Tuple of values for the fields of the pretty table"""
 
-    def new_student(self, cwid: int, name : str, major: str)-> None:
-        """add new student"""
+        for course,count in self.stud.items():
+            
+            yield self.cwid,self.name,self.dpt,course,count
 
-        self.student_dict[cwid] = Students(cwid, name, major)
+class Student:
+    """ Contains the info of the student and stores the information present in the file"""
 
-    def new_instructor(self, cwid: int, name: str, dept: str):
-        """add new instructor"""  
-        self.instructor_dict[cwid] = Instructors(cwid, name, dept)
-
-    def pretty_print_students(self):
-        """printing student data"""
-
-        student_table = PrettyTable()
-        student_table.field_names = ['CWID', 'Name', 'Completed Courses', 'Remaining Required', 'Remaining Elective']
-
-        for item in self.student_dict.values():
-            student_table.add_row([item.cwid, item.name, sorted(item.courses)])
-
-        print(student_table)
-
-    def pretty_print_instructors(self):
-        """printing instructor data"""
-
-        instructor_table = PrettyTable()
-        instructor_table.field_names = ['CWID', 'Name', 'Dept', 'course', 'students']
-
-        for item in self.instructor_dict.values():
-            for course, stu in item.courses.items():
-                instructor_table.add_row([item.cwid, item.name, item.dept, course, stu])
-
-        print(instructor_table)
-
-class Students(object):
-    """class for student"""
-
-    def __init__(self, cwid, name, major):
-        """init method for students"""
-
-        self.cwid: int = cwid
+    def __init__(self,cwid:str ,name:str ,major:str, required:list, electives: list):
+       
+        self.cwid: str = cwid
         self.name: str = name
         self.major: str = major
-        self.courses: str = list()
+        self.courselist:List[str] = list()
+        self.remaining_req: List[str] = required
+        self.remaining_elec: List[str] = electives
+        self.grade:  Dict[str,float] = {'A':4.0, 'A-':3.75, 'B+':3.25, 'B':3.0, 'B-':2.75, 'C+':2.25, 'C':2.0}
+        self.smry_st:  Dict[str,str] = dict()
 
-
-class Instructors(object):
-    """class for instructor"""
-
-    def __init__(self, cwid, name, dept):
-        """init method for instructors"""
-
-        self.cwid: int = cwid
-        self.name: str = name
-        self.dept: str = dept
-        self.courses: str = defaultdict(int)
-
-def file_reader(path : str, fields: int, sep: str, header=False):
-    """File Reader Function to clean a field separated file"""
-
-    if not os.path.exists(path):
-        raise FileNotFoundError
-
-    fp = open(path, 'r')
-
-    with open(path, 'r') as fp:
-        if header:
-            next(fp)
-
-        for n, line in enumerate(fp, 1):
-            line = line.strip()
-
-            if line.count(sep) == fields - 1:
-                yield list(line.split(sep))
-
+    def student_course(self, course: str, grade: str):
+        """Creates courses and grades dictionary"""
+        
+        self.smry_st[course] = grade
+        
+        if grade in self.grade.keys():
+        
+            self.courselist.append(course)    
+             
+        
+        else:
+        
+            return self.courselist
+    
+    def pretty_student(self) -> Tuple[str, str, List[str]]:
+        """calues for fields and tuples are returned"""
+       
+        return[self.cwid, self.name,self.major,sorted(self.courselist),sorted(self.calculate_required(self.smry_st)),sorted(self.calculate_electives(self.smry_st)),self.calculate_grade(self.smry_st)]
+    
+    def calculate_grade(self,dict:Dict[str,int]):
+        """This functiton calculates the grades of the student and converts it into GPA"""
+        
+        sum: int 
+        
+        crse_grade :List[int]= list()
+        for key, value in dict.items():
+            
+            if value == '':
+                continue
+            
+            if value in self.grade:
+                sum = sum + self.grade[value]
+                crse_grade.append(value)
+            
             else:
-                raise ValueError(f"expected {fields} fields"+
-                                 f"got {line.count(sep) + 1} on line {n}")
+                return 0.0
+        return format(sum/len(crse_grade),'.2f')
+    
+    def calculate_required(self,ls: Dict[str,str]):
+        """calculates the required courses"""
+        
+        crse_std: List[str] = [] #courses per student
+        
+        for key, value in ls.items():
+        
+            if value in self.grade.keys():
+                crse_std.append(key)
+        
+            else:
+                continue
+
+        
+        return list(set(self.remaining_req) - set(crse_std))
+    
+    def calculate_electives(self,ls: Dict[str,str]):
+        """electives are calculated"""
+        
+        crse_std: List[str] = []
+        
+        for key, value in ls.items():
+        
+            if value in self.grade.keys():
+                crse_std.append(key)
+        
+            else:
+                return(self.remaining_elec)
+        
+        if set(self.remaining_elec).intersection(crse_std):
+            return []
+        
+        else:
+            return list(self.remaining_elec)
+    
+
+    
 
 
 
-def main()-> None:
-    """ Main Function to interact with the user """
+class Repository:
+    """ info for student,major,instructors and grades"""
 
-    Stevens = Repository(r'C:/Users/shlokkhetan/Documents/SSW 810/810/homework 9')
+    def __init__(self,directory:str):
+        self.directory: str = directory
+        self.student: Dict[str,Student] = dict()
+        self.instructor: Dict[str,Instructor] = dict()
+        self.major: Dict[str,Major] = dict()
+ 
+       
+        try:
+            """Reading the files"""
+       
+            self._getmajors(os.path.join(directory,'major.txt'))
+            self._getstud(os.path.join(directory,'students.txt'))
+            self._getinstructor(os.path.join(directory,'instructors.txt'))
+            self._getgrade(os.path.join(directory,'grades.txt'))
 
-    Stevens.pretty_print_students()
+        except ValueError as verr:
+            
+            print("files are not matched by the number specified")
+        
+        except FileNotFoundError as fnerr:
+            
+            raise FileNotFoundError("file/directory not found")
+    
+    def _getmajors(self,directory: str) -> None:
+        """ major.txt file is read """
+        
+        for  major,R_crs,crs in file_reader(directory,3,sep='\t',header=True):
+        
+            if major not in self.major:
+                self.major[major] = Major(major)
+            self.major[major].addmajor(crs,R_crs)
+        
+    def _getstud(self,directory: str) -> None:
+        """student.txt file is read"""
+        
+        for cwid,name,major in file_reader(directory,3,sep=';',header=True):
+        
+            self.student[cwid] = Student(cwid,name,major,self.major[major].get_required(),self.major[major].get_electives())
+    
+    def _getinstructor(self,directory: str) -> None:
+        """instructor.txt file is read"""
+        
+        for cwid,name,dept in file_reader(directory,3,sep='|',header=True):
+           
+            self.instructor[cwid] = Instructor(cwid,name,dept)
+    
+    def _getgrade(self,directory: str) -> None:
+        """grade.txt file is read"""
+        
+        for stcwid,course,grade,inscwid in file_reader(directory,4,sep='|',header=True):
+        
+            if stcwid in self.student:
+                self.student[stcwid].student_course(course,grade)
+        
+            else:
+                print(f"unknown student grade '{stcwid}'")
+        
+            if inscwid in self.instructor:
+                self.instructor[inscwid].addstudent(course)
+        
+            else:
+                print(f"unknown instructor grade'{inscwid}'")
+    
+    
 
-    print("\n\n")
+    def pretty_print_st(self):
+        """student tale printed"""
 
-    Stevens.pretty_print_instructors()
+        ptstudent: PrettyTable = PrettyTable()
+        ptstudent.fieldnames = [
+            "CWID",
+            "Name",
+            "Major",
+            "Completed Course",
+            "Remaining Required",
+            "Remaining Electives",
+            "Grades"
+        ]
 
-    print(Stevens.instructor_dict)
+       
+        for student in self.student.values():
+            
+            ptstudent.add_row(student.prettystudent())
 
-if __name__ == '__main__':
-    main()
+        print('\n')
+        print("Student Summary")
+        print(ptstudent)
+
+    def pretty_print_ins(self):
+        """istructor tale printed """
+
+        ptinstructor: PrettyTable = PrettyTable()
+        
+        ptinstructor.fieldnames = [
+            "CWID",
+            "Name",
+            "Dept",
+            "Course",
+            "Students"
+        ]
+
+        
+        for instructor in self.instructor.values():
+            for row in instructor.prettyinstructor():
+                ptinstructor.add_row(row)
+
+        print('\n')
+        print("Instructor Summary")
+        print(ptinstructor)
+
+    def pretty_printmajor(self):
+        """major tale printed """
+
+        ptmajor: PrettyTable = PrettyTable()
+       
+        ptmajor.fieldnames = [
+            "Major",
+            "Required Courses",
+            "Electives"
+        ]
+
+        for majors in self.major.values():
+            ptmajor.add_row(majors.prettymajor())
+
+        print('\n')
+        print("Major Summary")
+        print(ptmajor)
+
+if name__ == '__main__':
+    r = Repository('C:\\Users\\shlokkhetan\\Desktop\\SSW_810\\HW10\\Student_Repository\\Student_Repository')
+    r.pretty_printmajor()
+    r.pretty_print_st()
+    r.pretty_print_ins()
---
+++ Test_Student_Repository_Shlok_Khetan.py
@@ -0,0 +1,57 @@
+"""unittest file to test HW10"""
+import unittest
+from Student_Repository_Shlok_Khetan import Repository, Student, Instructor
+import os, sys
+from prettytable import PrettyTable
+
+class Test_HW10(unittest.TestCase):
+    """ Class  to perform error checking ad handelling """
+    def test_class_instructor(self):
+        
+        stevens: Repository = Repository(r"C:\\Users\\shlokkhetan\\Desktop\\Second_Sem\\SSW_810\\HW10\\Student_Repository\\Student_Repository")
+        list1 = list()
+        list2 = [['98765', 'Einstein, A', 'SFEN', 'SSW 567', 4], ['98765', 'Einstein, A', 'SFEN', 'SSW 540', 3], ['98764', 'Feynman, R', 'SFEN', 'SSW 564', 3], ['98764', 'Feynman, R', 'SFEN', 'SSW 687', 3], ['98764', 'Feynman, R', 'SFEN', 'CS 501', 1], ['98764', 'Feynman, R', 'SFEN', 'CS 545', 1], ['98763', 'Newton, I', 'SFEN', 'SSW 555', 1], ['98763', 'Newton, I', 'SFEN', 'SSW 689', 1], ['98760', 'Darwin, C', 'SYEN', 'SYS 800', 1], ['98760', 'Darwin, C', 'SYEN', 'SYS 750', 1], ['98760', 'Darwin, C', 'SYEN', 'SYS 611', 2], ['98760', 'Darwin, C', 'SYEN', 'SYS 645', 1]]
+        for instructor in stevens.Instructor.values():
+            for row in instructor.pretty_instructor():
+                list1.append(list(row))
+
+        self.assertEqual(list1, list2)
+
+
+    def test_class_student(self):
+        
+        stevens: Repository = Repository(r"C:\\Users\\shlokkhetan\\Desktop\\Second_Sem\\SSW_810\\HW10\\Student_Repository\\Student_Repository")
+        list1 = list()
+        list2 = [['10103', 'Baldwin, C', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], '3.4'], ['10115', 'Wyatt, X', ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [],'3.8'], ['10172', 'Forbes, I', ['SSW 555', 'SSW 567'], ['SSW 540', 'SSW 564'], ['CS 501', 'CS 513', 'CS 545'], '3.9'], ['10175', 'Erickson, D', ['SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], ['CS 501', 'CS 513', 'CS 545'], '3.6'], ['10183', 'Chapman, O', ['SSW 689'], ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'], ['CS 501', 'CS 513', 'CS 545'], '4.0'], ['11399', 'Cordova, I', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], [], '3.0'], ['11461', 'Wright, U', ['SYS 611', 'SYS 750', 'SYS 800'], ['SYS 612', 'SYS 671'], ['SSW 540', 'SSW 565', 'SSW 810'], '3.9'], ['11658', 'Kelly, P', [], ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], 0.0], ['11714', 'Morton, A', ['SYS 611', 'SYS 645'], ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], '3.0'], ['11788', 'Fuller, E', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], [], '4.0']]
+        for student in stevens.Student.values():
+            list1.append(student.pretty_student())
+        
+        self.assertEqual(list1, list2)
+    
+    
+    def test_file_not_found_error(self) -> None:
+       
+        with self.assertRaises(FileNotFoundError):
+            Repository(r"C:\\Users\\shlokkhetan\\Desktop\\Second_Sem\\SSW_810\\HW09\\Nofile")
+
+   
+    
+    def test_class_major(self):
+       
+        stevens: Repository = Repository(r"C:\\Users\\shlokkhetan\\Desktop\\Second_Sem\\SSW_810\\HW10\\Student_Repository\\Student_Repository")
+        list1 = list()
+        list2 = [['SFEN', ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'], ['CS 501', 'CS 513', 'CS 545']], ['SYEN', ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810']]]
+        for major in stevens.Major.values():
+            list1.append(major.pretty_major())
+        # print(list1)
+        self.assertEqual(list1, list2)
+    
+    def test_file_not_found_error(self) -> None:
+        
+        with self.assertRaises(FileNotFoundError):
+            Repository(r"C:\\Users\\shlokkhetan\\Desktop\\Second_Sem\\SSW_810\\HW09\\Nofile")
+
+        
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
